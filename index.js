8f43bc131f1ce6520ac7303ed5d626357b383d7b
miriam <miriamwangui525@gmail.com>
2023-12-15 00:18:23 +0300

    how to loop in js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+function writeCards(names, occasion) {
+    const thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+      thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${occasion} gift!`);
+    }
+    return thankYouMessages;
+  }
+  
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = {
+      writeCards,
+      countDown,
+    };
+  }
+  
